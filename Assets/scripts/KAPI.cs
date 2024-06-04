2c4f19a3e7ba77af8f0591281bca5ad7351fe8b7
Ozankibu <efesanli01@gmail.com>
2024-06-05 01:58:26 +0300

    Commit en son hali bu sex

--- Assets/scripts/KAPI.cs
+++ Assets/scripts/KAPI.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class KAPI : MonoBehaviour, IInteractable
 {
+    public bool isopen = false;
     public void Interact()
     {
-        Debug.Log("1");
-        GetComponent<Animator>().SetBool("isopen", true);
+        if (isopen == false)
+        {
+            GetComponent<Animator>().SetTrigger("isopen");
+            isopen = true;
+        }
+        else if (isopen == true)
+        {
+            GetComponent<Animator>().SetTrigger("close");
+            isopen = false;
+        }
 
     }
    
